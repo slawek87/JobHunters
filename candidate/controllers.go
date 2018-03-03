5a675f186328a05938138870bb026a980c94cb19
Sławek Kabik <slawek@thecapitals.nl>
2018-03-03 18:03:10 +0100

    basic controllers

--- candidate/controllers.go
+++ candidate/controllers.go
@@ -1,1 +1,28 @@
 package candidate
+
+
+const MongoDBIndex = "Candidate"
+
+type CandidateController struct {
+	Candidate Candidate
+}
+
+func (controller *CandidateController) SetCandidate(candidate Candidate) {
+	controller.Candidate = candidate
+}
+
+func (controller *CandidateController) SetCandidateID(candidateID string) {
+	controller.Candidate.CandidateID = candidateID
+}
+
+func (controller *CandidateController) SetOfferID(OfferID string) {
+	controller.Candidate.OfferID = OfferID
+}
+
+func (controller *CandidateController) SetRecruiterID(RecruiterID string) {
+	controller.Candidate.RecruiterID = RecruiterID
+}
+
+func (controller *CandidateController) GetCandidate() Candidate {
+	return controller.Candidate
+}
